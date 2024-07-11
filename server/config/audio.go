2f32e0dc56082f595215976f9a843392ea02bba7
hyouka <838611931@qq.com>
2024-07-11 18:00:09 +0800

    feat: 修改代码

--- server/config/audio.go
+++ server/config/audio.go
@@ -1,9 +1,8 @@
 package config
 
 type Audio struct {
-	Voice    string `yaml:"voice"`     // 角色名称
-	Rate     string `yaml:"rate"`      // 语速
-	Volume   string `yaml:"volume"`    // 音量
-	Pitch    string `yaml:"pitch"`     // 分贝
-	ProxyUrl string `yaml:"proxy_url"` // 代理路径
+	Voice  string `yaml:"voice"`  // 角色名称
+	Rate   string `yaml:"rate"`   // 语速
+	Volume string `yaml:"volume"` // 音量
+	Pitch  string `yaml:"pitch"`  // 分贝
 }
